a69d061dbf60dca192332feb6689ba0a5a1093e7
Thomas Backman <serenity@exscape.org>
2011-08-31 22:34:13 +0200

    First commit! It works!

---
+++ kernel.c
@@ -0,0 +1,67 @@
+int strlen(const char *str) {
+	int len = 0;
+	const char *p = str;
+	while (*p++ != 0) {
+		len++;
+	}
+
+	return len;
+}
+
+void panic(const char *str) {
+   unsigned char *videoram = (unsigned char *) 0xb8000;
+
+	int len = strlen(str);
+
+	int i = 0;
+
+	// Clear screen
+	for (i = 0; i < 80*25 * 2; i++) {
+		videoram[i] = 0;
+	}
+
+	for (i = 0; i < len; i++) {
+		videoram[2*i] = str[i];
+		videoram[2*i+1] = 0x07;
+	}
+
+	// FIXME: halt, somehow
+}
+void kmain( void* mbd, unsigned int magic )
+{
+   if ( magic != 0x2BADB002 )
+   {
+      /* Something went not according to specs. Print an error */
+      /* message and halt, but do *not* rely on the multiboot */
+      /* data structure. */
+   }
+ 
+   mbd = mbd; // silence warning
+
+   /* You could either use multiboot.h */
+   /* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
+   /* or do your offsets yourself. The following is merely an example. */ 
+   //char * boot_loader_name =(char*) ((long*)mbd)[16];
+ 
+   /* Print a letter to screen to see everything is working: */
+//   videoram[0] = 65; /* character 'A' */
+//   videoram[1] = 0x07; /* light grey (7) on black (0). */
+/*
+	char *greeting = "Hello world!";
+	int greeting_len = strlen(greeting);
+
+	int i = 0;
+
+	// Clear screen
+	for (i = 0; i < 80*25 * 2; i++) {
+		videoram[i] = 0;
+	}
+
+	for (i = 0; i < greeting_len; i++) {
+		videoram[2*i] = greeting[i];
+		videoram[2*i+1] = 0x07;
+	}
+	*/
+
+   panic("Hello, world! I panic!");
+}
