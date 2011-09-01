36c3670a2268beaa8a37b04e07965f35719ac6e8
Thomas Backman <serenity@exscape.org>
2011-09-01 21:46:36 +0200

    Dirty commit; do SOME work on get_time(), more work on print(), neither works well!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,28 @@
 #include <types.h>
 
+// TODO: Proper makefile for .s -> .o and linking
+
 static unsigned char *videoram = (unsigned char *) 0xb8000;
 static Point cursor;
 
+typedef struct Time {
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
+} Time;
+
+
+int putchar(int c);
 void *memset(void *addr, int c, size_t n);
 size_t strlen(const char *str);
 void panic(const char *str);
 void print(const char *str);
 void clrscr(void);
 void kmain(void* mbd, unsigned int magic);
+void get_time(Time *);
 
 void *memset(void *addr, int c, size_t n) {
 	unsigned char *p = addr;
@@ -32,26 +46,91 @@ size_t strlen(const char *str) {
 
 void clrscr(void) {
 	memset(videoram, 0, 80*25*2);
+	cursor.x = 0;
+	cursor.y = 0;
+}
+
+int putchar(int c) {
+	char out[2] = {0};
+	out[0] = (unsigned char)c;
+	print(out);
+
+	return c;
 }
 
 void print(const char *str) {
-	const size_t len = strlen(str);
-
-	const unsigned int offset = cursor.y*80*2 + cursor.x*2;
+	size_t len = strlen(str);
 
 	for (size_t i = 0; i < len; i++) {
-		videoram[2*i +   offset] = (unsigned char)str[i]; // FIXME: does this work as expected?
-		videoram[2*i+1 + offset] = 0x07;
+		const unsigned int offset = cursor.y*80*2 + cursor.x*2;
+
+		if (str[i] != '\n') {
+			videoram[2*i +   offset] = (unsigned char)str[i]; // FIXME: does this work as expected?
+			videoram[2*i+1 + offset] = 0x07;
+		}
+		else {
+			cursor.x = 0;
+			cursor.y++;
+			continue;
+		}
 	}
 
+	// Remove newline characters from the calculations
+	for (size_t i = len-1; i >= 0; i--) {
+		if (i == (size_t)-1)
+			break;
+		if (str[i] == '\n')
+			len--;
+	}
+
+	if (cursor.x + len > 80) {
+		cursor.y += (cursor.x+len)/80;
+	}
+	
 	cursor.x += len;
+
+	if (cursor.x > 80)
+		cursor.x = cursor.x % 80;
+
+	videoram[cursor.y*80*2 + cursor.x*2] = 178;
+	videoram[cursor.y*80*2 + cursor.x*2 + 1] = 0x7;
+
+	// TODO:
+	// Wrapping!
+	// Track cursor position, w/ wrapping
 }
 
 void panic(const char *str) {
 	clrscr();
 	print("PANIC: ");
 	print(str);
+	asm("hangloop: hlt ; jmp hangloop");
 	// FIXME: halt, somehow
+}
+
+void get_time(Time *t) {
+	t->year = 2011;
+
+	unsigned char sec = 0;
+
+	asm(
+		".ll:"
+		"movb $10, %%al;"
+		"outb %%al, $0x70;"
+		"inb $0x71, %%al;"
+		"testb $0x80, %%al;"
+		"jne .ll;"
+
+		"movb $0x02, %%al;"
+		"outb %%al, $0x70;"
+		"inb $0x71, %%al;"
+		"movb %%al, %0;"
+		: "=r"(sec) : : "%al");
+
+	// TODO: Check if BCD or not
+	sec = ((sec / 16) * 10) + (unsigned char)(sec & (unsigned char)0xf);
+
+	print("Break here");
 }
 
 void kmain(void* mbd, unsigned int magic) {
@@ -74,5 +153,11 @@ void kmain(void* mbd, unsigned int magic) {
    cursor.x = 0;
    cursor.y = 0;
 
-	print("Hello world!");
+   print("Hello world! Printing works, and keeping track of the cursor as well! Blah blah");
+   print("\nThis is on a new line.\n");
+   print("This is also on a new line.\n this too!");
+
+   Time t;
+   memset(&t, 0, sizeof(t));
+   get_time(&t);
 }
