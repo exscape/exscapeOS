64f064143b01b0bb45baac0ab7ea1a7c2ff0a05c
Thomas Backman <serenity@exscape.org>
2011-09-02 22:10:30 +0200

    Delete run.sh; it is not used any longer.

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp kernel.bin isofiles/boot &&
-mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o bootable.iso isofiles &&
-/Applications/Q.app/Contents/MacOS/i386-softmmu.app/Contents/MacOS/i386-softmmu -cdrom bootable.iso -boot d1 
