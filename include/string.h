679f05586efb115e3ac0cfb5113c4d19a3d77959
Thomas Backman <serenity@exscape.org>
2011-09-03 14:36:44 +0200

    Add memcpy()

--- include/string.h
+++ include/string.h
@@ -5,5 +5,6 @@
 
 void *memset(void *addr, int c, size_t n);
 size_t strlen(const char *str);
+void *memcpy(void *restrict s1, const void *restrict s2, size_t n);
 
 #endif
--- lib/string.c
+++ lib/string.c
@@ -19,3 +19,17 @@ size_t strlen(const char *str) {
 
 	return len;
 }
+
+void *memcpy(void *restrict s1, const void *restrict s2, size_t n) {
+	// Since we can't dereference a void pointer...
+	char *p1 = s1;
+	const char *p2 = s2;
+
+	while (n-- > 0) {
+		*p1++ = *p2++;
+	}
+
+	// Return value should be the original s1 pointer
+	return s1;
+}
+
