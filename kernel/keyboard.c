20b3d898050f6bfbd85bb9ce887f0390c3951fbc
Thomas Backman <serenity@exscape.org>
2011-09-04 15:22:55 +0200

    Print keycode; test commit (I moved the git repo locally)

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -1,10 +1,12 @@
 #include <types.h>
 #include <gdtidt.h> /* isr_t, registers_t */
 #include <kernutil.h> /* panic */
+#include <monitor.h>
 
 void register_interrupt_handler(uint8 n, isr_t handler);
 
 void keyboard_callback(registers_t regs) {
-	panic("Keyboard!");
-	regs = regs;
+	regs = regs; // avoid a warning
+	unsigned char scancode = inb(0x60);
+	printk("%x ", scancode);
 }
