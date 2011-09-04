0b0215cc7d9fcb81919fd9ef38617bd37c8d4217
Thomas Backman <serenity@exscape.org>
2011-09-04 21:28:14 +0200

    Finish the RTC code; well, it still doesn't handle timezones... It does however handle 12h time and 24h time, in binary or BCD format, and displays the same end result in any of the four cases... As far asd I can tell.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <keyboard.h>
 
-#define DIVZERO_10_SEC /* divides by zero every 10 second, to test exceptions */
+//#define DIVZERO_10_SEC /* divides by zero every 10 second, to test exceptions */
 
 // TODO: Proper makefile for .s -> .o and linking
 
@@ -17,6 +17,59 @@ void get_time(Time *t) {
 	/* This function isn't pretty, at all, but it didn't appear very easy to share
 	   structs between nasm and C... So I chose inline assembly. */
 	unsigned char yeartmp = 0;
+
+
+	/*
+	 * This is a big chunk of code used for debugging the RTC routines.
+	 * It changes status register B of the CMOS to change the use
+	 * of 12h/24h time and BCD/binary representations.
+	 * It's still here just in case; rewriting it would be boring.
+	 */
+
+/*	
+	// Set status reg B for debugging purposes
+	unsigned char rb = 0;
+
+	asm(
+	// wait until no update is in progress
+		".lltmp:"
+		"movb $10, %%al;"
+		"outb %%al, $0x70;"
+		"inb $0x71, %%al;"
+		"testb $0x80, %%al;"
+		"jne .lltmp;"
+
+	// fetch status reg B
+		"movb $0xb, %%al;" // status reg B
+		"outb %%al, $0x70;"
+		"inb $0x71, %%al;"
+		"movb %%al, %[rb];"
+		: [rb]"=m"(rb) : : "%al", "memory");
+
+	// Force 12h time
+	rb &= ~2;
+
+	// set status reg B
+	asm("cli;"
+		"movb $0xb, %%al;"
+		"outb %%al, $0x70;"
+		"movb %[rb], %%al;"
+		"nop;nop;nop;nop;nop;nop;nop;nop;"
+		"outb %%al, $0x71;"
+		"sti;"
+		: : [rb]"m"(rb) : "%al", "memory");
+
+	// Set the clock (to debug AM/PM issues)
+	asm("cli;"
+		"movb $0x4, %%al;" // set the hour...
+		"outb %%al, $0x70;"
+		"movb $0, %%al;"  // ...to the time specified
+		"nop;nop;nop;nop;nop;nop;nop;nop;"
+		"outb %%al, $0x71;"
+		"sti;"
+		: : : "%al", "memory");
+*/
+	// END DEBUGGING CODE
 
 	asm(
 		/* make sure the update flag isn't set */
@@ -74,7 +127,7 @@ void get_time(Time *t) {
 
 	/* Fetch CMOS status register B */
 	unsigned char regb;
-	asm("movb $0xb, %%al;" // status reg B
+	asm("movb $0xb, %%al;" // 0xb = status reg B
 		"outb %%al, $0x70;"
 		"inb $0x71, %%al;"
 		"movb %%al, %[regb];"
@@ -89,15 +142,41 @@ void get_time(Time *t) {
 	is_bcd     = (regb & 4) ? 0 : 1; /* [sic] */
 	is_24_hour = (regb & 2) ? 1 : 0; 
 
+	uint8 time_is_pm = 0;
+
+	/*
+	 * If this is a 12-hour clock, mask out the 0x80 bit if it's set.
+	 * If it is, the time is PM. If not, it's AM.
+	 * This is used below, after BCD decoding.
+	 */
+	if (!is_24_hour) {
+		if ((t->hour & 0x80) != 0) {
+			// Remove the bit (to make the hour readable)
+			t->hour &= ~0x80;
+			// Save this knowledge, though; it's used below
+			time_is_pm = 1;
+		}
+	}
+
+	/* Convert the data from BCD to binary form, if needed */
 	if (is_bcd) {
 #define bcd_to_bin(x) ((x / 16) * 10) + (unsigned char)(x & (unsigned char)0xf)
-		// Convert from BCD to decimal form
 		yeartmp   = bcd_to_bin(yeartmp);
 		t->month  = bcd_to_bin(t->month);
 		t->day    = bcd_to_bin(t->day);
 		t->hour   = bcd_to_bin(t->hour);
 		t->minute = bcd_to_bin(t->minute);
 		t->second = bcd_to_bin(t->second);
+	}
+
+	if (!is_24_hour) {
+		/* Midnight is stored as 12PM; we want to display it as 00 */
+		if (time_is_pm && t->hour == 12)
+			t->hour = 0;
+		else if (time_is_pm) {
+			/* Otherwise, just move it to 24-hour times, as usual */
+			t->hour += 12;
+		}
 	}
 
 /* Used to debug exception handling */
@@ -109,11 +188,11 @@ void get_time(Time *t) {
 	}
 #endif
 
-	if (!is_24_hour) {
-		// TODO: Get PM flag and adjust t->hour
-	}
-
-	t->year = 2000 + yeartmp; // TODO: get century from RTC (in case this lives on for 89+ years! ;)
+	/*
+	 * This will fail for year 2100+, and I frankly don't care enough to fix it.
+	 * In fact, I would if I was *sure* it was always stored it register 0x32.
+	 */
+	t->year = 2000 + yeartmp; 
 }
 
 void kmain(void* mbd, unsigned int magic) {
