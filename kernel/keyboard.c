a0bee2ab13190d74a8d7c265c54d5dae688ca148
Thomas Backman <serenity@exscape.org>
2011-09-05 09:58:53 +0200

    Keymap updates; allow the basic alt+ characters, such as {} [] @ $

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -12,10 +12,10 @@ static unsigned char mod_keys = 0;
 #define MOD_ALT   (1 << 2)
 
 /* A US keymap, courtesy of Bran's tutorial */
-unsigned char kbdus[128] =
+unsigned char kbdmix[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
+  '9', '0', '+', /*'´' FIXME */0, '\b',	/* Backspace */
   '\t',			/* Tab */
   'q', 'w', 'e', 'r',	/* 19 */
   't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
@@ -23,7 +23,7 @@ unsigned char kbdus[128] =
   'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
  '\'', '`',   0,		/* Left shift */
  '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
+  'm', ',', '.', '-',   0,				/* Right shift */
   '*',
     0,	/* Alt */
   ' ',	/* Space bar */
@@ -76,6 +76,46 @@ unsigned char kbdse_shift[128] =
  '>', 'Z', 'X', 'C', 'V', 'B', 'N',            /* 49 */
   'M', ';', ':', '_',   0,              /* Right shift */
 
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};		
+
+unsigned char kbdse_alt[128] =
+{
+    0,  27, 0 /*alt+1*/, '@', 0, '$', 0, 0, '{', '[',	/* 9 */
+  ']', '}', '\\', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
   '*',
     0,	/* Alt */
   ' ',	/* Space bar */
@@ -172,11 +212,15 @@ void keyboard_callback(registers_t regs) {
 
 	if (mod_keys == MOD_NONE && !(scancode & 0x80)) {
 		// No modifiers
-		c = kbdus[scancode];
+		c = kbdmix[scancode];
 	}
 	else if (mod_keys == MOD_SHIFT && !(scancode & 0x80)) {
 		// Shift + key
 		c = kbdse_shift[scancode];
+	}
+	else if (mod_keys == MOD_ALT && !(scancode & 0x80)) {
+		// Alt + key
+		c = kbdse_alt[scancode];
 	}
 
 /* TODO: alt + number keys for @, [, ] etc. */
