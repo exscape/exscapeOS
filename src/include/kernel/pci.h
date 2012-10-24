f582fee12330db7c3721199004dd78653107c18c
Thomas Backman <serenity@exscape.org>
2012-10-24 09:47:41 +0200

    Create a PCI database in init_pci(); provide an interface to access it

--- src/include/kernel/pci.h
+++ src/include/kernel/pci.h
@@ -1,33 +1,51 @@
 #ifndef _PCI_H
 #define _PCI_H
 
+typedef struct {
+	uint32 address;
+	uint8 type; /* BAR_MEM or BAR_IO */
+} bar_t;
+
+typedef struct {
+	uint16 vendor_id;
+	uint16 device_id;
+	bar_t bar[6];
+	uint8 irq;
+} pci_device_t;
+
+// Looks through the PCI database to find the details (BARs, IRQ etc.)
+// for a PCI device
+pci_device_t *find_pci_device(uint32 vendor_id, uint32 device_id);
+
 /* The IO registers used to access PCI registers */
 #define PCI_CONFIG_ADDRESS 0xcf8
 #define PCI_CONFIG_DATA 0xcfc
 
-
 /* PCI configuration space offsets
- * NOTE: DEVICE and VENDOR share the same offset, because
- * they are part of the same DWORD. AND:ing and shifting must be done!
+ * comments after the value state what to AND the 32-bit register with 
+ * to get the final value
  */
-
-#define PCI_CONF_DEVICE			0x00
-#define PCI_CONF_VENDOR			0x00
-#define PCI_CONF_STATUS			0x04
-#define PCI_CONF_COMMAND		0x04
-#define PCI_CONF_CLASS			0x08
-#define PCI_CONF_REVISION		0x08
-#define PCI_CONF_BIST			0x0c
-#define PCI_CONF_HEADER_TYPE	0x0c
-#define PCI_CONF_LATENCY		0x0c
-#define PCI_CONF_CACHELINE_SIZE 0x0c
+#define PCI_CONF_DEVICE			0x00 /* 0xffff0000 */
+#define PCI_CONF_VENDOR			0x00 /* 0x0000ffff */
+#define PCI_CONF_STATUS			0x04 /* 0xffff0000 */
+#define PCI_CONF_COMMAND		0x04 /* 0x0000ffff */
+#define PCI_CONF_CLASS			0x08 /* 0xffffff00 */
+#define PCI_CONF_REVISION		0x08 /* 0x000000ff */
+#define PCI_CONF_BIST			0x0c /* 0xff000000 */
+#define PCI_CONF_HEADER_TYPE	0x0c /* 0x00ff0000 */
+#define PCI_CONF_LATENCY		0x0c /* 0x0000ff00 */
+#define PCI_CONF_CACHELINE_SIZE 0x0c /* 0x000000ff */
 #define PCI_CONF_BAR0			0x10
 #define PCI_CONF_BAR1			0x14
 #define PCI_CONF_BAR2			0x18
 #define PCI_CONF_BAR3			0x1c
 #define PCI_CONF_BAR4			0x20
 #define PCI_CONF_BAR5			0x24
-/* anything later is not used yet */
+#define PCI_CONF_IRQ			0x3C /* 0x000000ff */
+
+/* bit 0 has these values for the two BAR types */
+#define BAR_MEM 0
+#define BAR_IO 1
 
 #define PCI_ENABLE (1UL << 31)
 
--- src/kernel/pci.c
+++ src/kernel/pci.c
@@ -1,6 +1,8 @@
 #include <types.h>
 #include <kernel/pci.h>
 #include <kernel/kernutil.h>
+#include <kernel/timer.h>
+#include <string.h> /* memset */
 
 /* Is this used? */
 #include <kernel/kheap.h>
@@ -8,20 +10,37 @@
 /* For debugging */
 #include <kernel/console.h>
 
+list_t *pci_devices = NULL;
+
+pci_device_t *find_pci_device(uint32 vendor_id, uint32 device_id) {
+	// list_node_find_next_predicate can't accept additional parameters (d'oh!),
+	// so we'll have to do this ourselves.
+	for (node_t *it = pci_devices->head; it != NULL; it = it->next) {
+		pci_device_t *cur = (pci_device_t *)it->data;
+		if (cur->vendor_id == vendor_id && cur->device_id == device_id)
+			return cur;
+
+	}
+
+	return NULL;
+}
+
+// Reads the PCI configuration space, one dword at a time.
+// To read 8 or 16 bits, you currently need to AND the rest out yourself.
 uint32 read_pci_config(uint8 bus, uint8 slot, uint8 func, uint8 reg) {
 	outl(PCI_CONFIG_ADDRESS, PCI_ENABLE | (bus << 16) | (slot << 11) | (func << 8) | (reg & ~0x3));
 	return inl(PCI_CONFIG_DATA);
-//	return (unsigned short)((inl (0xCFC) >> ((offset & 2) * 8)) & 0xffff);
 }
 
 void init_pci(void) {
 	uint32 bus, slot, func;
 
+	pci_devices = list_create();
+
 	for (bus = 0; bus < 256; bus++) {
 		for (slot = 0; slot < 32; slot++) {
 			for (func = 0; func < 8; func++) {
 				uint32 vendor_id, device_id;
-				uint32 bar0, bar1, bar2, bar3, bar4, bar5;
 
 				vendor_id = (read_pci_config(bus, slot, func, PCI_CONF_VENDOR) & 0xffff);
 				if (vendor_id == 0xffff)
@@ -30,20 +49,62 @@ void init_pci(void) {
 				device_id = (read_pci_config(bus, slot, func, PCI_CONF_DEVICE) & 0xffff0000) >> 16;
 
 				printk("device found at bus %u, slot %u, func %u: vendor 0x%04x, device 0x%04x\n", bus, slot, func, vendor_id, device_id);
+				uint8 type = (read_pci_config(bus, slot, func, PCI_CONF_HEADER_TYPE) & 0x00ff0000) >> 24;
+				if (type == 0)
+					printk("Type: regular PCI device\n");
+				else if (type == 1)
+					printk("Type: PCI-to-PCI bridge\n");
+				else if (type == 2)
+					printk("Type: CardBus bridge\n");
 
-				if (/* header type == 00 */ true) {
-					bar0 = read_pci_config(bus, slot, func, 0x10);
-					bar1 = read_pci_config(bus, slot, func, 0x14);
-					bar2 = read_pci_config(bus, slot, func, 0x18);
-					bar3 = read_pci_config(bus, slot, func, 0x1c);
-					bar4 = read_pci_config(bus, slot, func, 0x20);
-					bar5 = read_pci_config(bus, slot, func, 0x24);
+				uint8 interrupt = read_pci_config(bus, slot, func, PCI_CONF_IRQ) & 0xff; // lower 8 bits is the interrupt
+				printk("IRQ: %u\n", interrupt);
 
-					printk(" bar0 0x%08x bar1 0x%08x bar2 0x%08x bar3 0x%08x bar4 0x%08x bar5 0x%08x\n", bar0, bar1, bar2, bar3, bar4, bar5);
+				// Store this device in the PCI device database
+				pci_device_t *dev = kmalloc(sizeof(pci_device_t));
+				memset(dev, 0, sizeof(pci_device_t));
+				dev->vendor_id = vendor_id;
+				dev->device_id = device_id;
+				dev->irq = interrupt;
+
+				list_append(pci_devices, dev);
+				// BARs are filled in below
+
+				uint32 bar[6] = {0};
+				if (type == 0) {
+					for (int i=0; i<6; i++) {
+						bar[i] = read_pci_config(bus, slot, func, 0x10 + i*4);
+						if ((bar[i] & 1) == BAR_IO) {
+							// This is an I/O space address; the 2 LSBs aren't part of the address
+							dev->bar[i].type = BAR_IO;
+							dev->bar[i].address = bar[i] & ~0x3;
+							printk("BAR%d is an IO address\n", i);
+						}
+						else {
+							// Memory space address; 4 LSBs aren't part of the address (bit 3 is prefetch, 2-1 type, and bit 0 always 0)
+							dev->bar[i].type = BAR_MEM;
+							dev->bar[i].address = bar[i] & ~0xf;
+							if (bar[i] != 0) /* used for empty BARs */
+								printk("BAR%d is a memory address\n", i);
+						}
+					}
+
+					printk(" bar0 0x%08x bar1 0x%08x bar2 0x%08x bar3 0x%08x bar4 0x%08x bar5 0x%08x\n", dev->bar[0].address, dev->bar[1].address, dev->bar[2].address, dev->bar[3].address, dev->bar[4].address, dev->bar[5].address);
+					printk("\n");
+					printk("database size is now %u items\n", pci_devices->count);
 				}
 			}
 		}
 	}
 
-	panic("halt");
+	for (node_t *n = pci_devices->head; n != NULL; n = n->next) {
+		pci_device_t *cur = (pci_device_t *)n->data;
+		printk("%04x:%04x, IRQ %u", cur->vendor_id, cur->device_id, cur->irq);
+		for (int i=0; i<6; i++) {
+			if (cur->bar[i].address != 0) {
+				printk(" BAR%u (%s): 0x%08x", i, cur->bar[i].type == BAR_IO ? "IO" : "MEM", cur->bar[i].address);
+			}
+		}
+		printk("\n");
+	}
 }
