65d8b0a28712b8fe58ba95365c08d0316f02d050
Thomas Backman <serenity@exscape.org>
2012-10-27 19:42:26 +0200

    Quiet down PCI printouts a bit

--- src/kernel/pci.c
+++ src/kernel/pci.c
@@ -48,17 +48,20 @@ void init_pci(void) {
 
 				device_id = (pci_read_config(bus, slot, func, PCI_CONF_DEVICE) & 0xffff0000) >> 16;
 
-				printk("device found at bus %u, slot %u, func %u: vendor 0x%04x, device 0x%04x\n", bus, slot, func, vendor_id, device_id);
+				//printk("device found at bus %u, slot %u, func %u: vendor 0x%04x, device 0x%04x\n", bus, slot, func, vendor_id, device_id);
 				uint8 type = (pci_read_config(bus, slot, func, PCI_CONF_HEADER_TYPE) & 0x00ff0000) >> 24;
+
+				/*
 				if (type == 0)
 					printk("Type: regular PCI device\n");
 				else if (type == 1)
 					printk("Type: PCI-to-PCI bridge\n");
 				else if (type == 2)
 					printk("Type: CardBus bridge\n");
+				*/
 
 				uint8 interrupt = pci_read_config(bus, slot, func, PCI_CONF_IRQ) & 0xff; // lower 8 bits is the interrupt
-				printk("IRQ: %u\n", interrupt);
+				//printk("IRQ: %u\n", interrupt);
 
 				// Store this device in the PCI device database
 				pci_device_t *dev = kmalloc(sizeof(pci_device_t));
@@ -78,22 +81,22 @@ void init_pci(void) {
 							// This is an I/O space address; the 2 LSBs aren't part of the address
 							dev->bar[i].type = BAR_IO;
 							dev->bar[i].address = bar[i] & ~0x3;
-							printk("BAR%d is an IO address\n", i);
+							//printk("BAR%d is an IO address\n", i);
 						}
 						else {
 							// Memory space address; 4 LSBs aren't part of the address (bit 3 is prefetch, 2-1 type, and bit 0 always 0)
 							dev->bar[i].type = BAR_MEM;
 							dev->bar[i].address = bar[i] & ~0xf;
-							if (bar[i] != 0) /* used for empty BARs */
-								printk("BAR%d is a memory address\n", i);
+							//if (bar[i] != 0) /* used for empty BARs */
+								//printk("BAR%d is a memory address\n", i);
 						}
 					}
 
-					printk("PCI revision: 0x%02x\n", pci_read_config(bus, slot, func, PCI_CONF_REVISION) & 0xff);
+					//printk("PCI revision: 0x%02x\n", pci_read_config(bus, slot, func, PCI_CONF_REVISION) & 0xff);
 
-					printk(" bar0 0x%08x bar1 0x%08x bar2 0x%08x bar3 0x%08x bar4 0x%08x bar5 0x%08x\n", dev->bar[0].address, dev->bar[1].address, dev->bar[2].address, dev->bar[3].address, dev->bar[4].address, dev->bar[5].address);
-					printk("\n");
-					printk("database size is now %u items\n", pci_devices->count);
+					//printk(" bar0 0x%08x bar1 0x%08x bar2 0x%08x bar3 0x%08x bar4 0x%08x bar5 0x%08x\n", dev->bar[0].address, dev->bar[1].address, dev->bar[2].address, dev->bar[3].address, dev->bar[4].address, dev->bar[5].address);
+					//printk("\n");
+					//printk("database size is now %u items\n", pci_devices->count);
 				}
 			}
 		}
@@ -101,7 +104,9 @@ void init_pci(void) {
 
 	for (node_t *n = pci_devices->head; n != NULL; n = n->next) {
 		pci_device_t *cur = (pci_device_t *)n->data;
-		printk("%04x:%04x, IRQ %u", cur->vendor_id, cur->device_id, cur->irq);
+		printk("%04x:%04x", cur->vendor_id, cur->device_id);
+		if (cur->irq != 0)
+			printk(", IRQ %u", cur->irq);
 		for (int i=0; i<6; i++) {
 			if (cur->bar[i].address != 0) {
 				printk(" BAR%u (%s): 0x%08x", i, cur->bar[i].type == BAR_IO ? "IO" : "MEM", cur->bar[i].address);
