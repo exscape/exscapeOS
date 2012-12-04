82e809fc0949847463bf0efdeffe3886349a258f
Thomas Backman <serenity@exscape.org>
2012-12-04 20:17:22 +0100

    Fix NULL pointer crash

--- src/kernel/initrd.c
+++ src/kernel/initrd.c
@@ -124,6 +124,8 @@ int initrd_open(uint32 dev, const char *path, int mode) {
 		assert(p == NULL); // path much not contain directories
 		p = path + 1;
 	}
+	else
+		p = path;
 
 	assert(current_task->_next_fd + 1 <= MAX_OPEN_FILES);
 	struct open_file *file = (struct open_file *)&current_task->fdtable[current_task->_next_fd++];
