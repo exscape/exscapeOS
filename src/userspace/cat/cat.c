21763ba2d5ab7137f29af4cb65e06270dccc5ca1
Thomas Backman <serenity@exscape.org>
2012-12-05 20:28:39 +0100

    cat: Don't mess up paths (that was a temporary test)

--- src/userspace/cat/cat.c
+++ src/userspace/cat/cat.c
@@ -6,29 +6,16 @@ int main(int argc, char **argv) {
 	if (argc == 1)
 		return 0;
 
-	char buf[512] = {0};
+	char buf[512];
 
 	for (int i=1; i < argc; i++) {
-		if (argv[i][0] != '/') {
-			buf[0] = '/';
-			for (int j=0;; j++) {
-				if (argv[i][j] != 0)
-					buf[1+j] = argv[i][j];
-				else {
-					buf[1+j] = 0;
-					break;
-				}
-			}
-		}
-
-		int fd = open(buf, O_RDONLY);
+		int fd = open(argv[i], O_RDONLY);
 		if (fd < 0) {
 			puts("error opening ");
 			puts(argv[i]);
 			puts(", exiting\n");
 			return 0;
 		}
-
 
 		int r = 0;
 		do {
