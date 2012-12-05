8d7cddd6884c76ea3724919edd7330aa3fd478d4
Thomas Backman <serenity@exscape.org>
2012-12-05 20:03:09 +0100

    Move path calculations to a separate function.
    Less code duplication means 22 fewer lines of code, not to mention that the work
    in adding support for relative paths to syscalls will reduce by two thirds.

--- src/kernel/fileio.c
+++ src/kernel/fileio.c
@@ -12,13 +12,16 @@
 void *devtable[MAX_DEVS] = {0};
 uint32 next_dev = 0;
 
-DIR *opendir(const char *path) {
+static bool find_relpath(const char *path, char *relpath, mountpoint_t **mp_out) {
+	// Transforms the path to a function (currently open, opendir and stat)
+	// into a path relative to the mountpoint. E.g. /initrd/mounts would turn in to
+	// just "/mounts" if the initrd is mounted under /initrd, and the initrd would then
+	// open /mounts seen from the "initrd root".
+
 	assert(path != NULL);
-
 	if (path[0] != '/')
-		return NULL; // only absolute paths are supported
+		return false; // only absolute paths are supported
 
-	char relpath[1024] = {0};
 	mountpoint_t *mp = find_mountpoint_for_path(path);
 	assert(mp != NULL);
 
@@ -33,49 +36,41 @@ DIR *opendir(const char *path) {
 			strcpy(relpath, "/");
 	}
 
-	assert(mp->fops.opendir != NULL);
+	*mp_out = mp;
+	return true;
+}
 
+DIR *opendir(const char *path) {
+	char relpath[1024] = {0};
+	mountpoint_t *mp = NULL;
+	if (!find_relpath(path, relpath, &mp))
+		return NULL;
+
+	assert(mp->fops.opendir != NULL);
 	return mp->fops.opendir(mp, relpath);
 }
 
-struct dirent *readdir(DIR *dir) {
-	assert(dir != NULL);
-	assert(dir->mp != NULL);
-	return dir->mp->fops.readdir(dir);
-}
-
-int closedir(DIR *dir) {
-	assert(dir != NULL);
-	assert(dir->mp != NULL);
-	return dir->mp->fops.closedir(dir);
-}
-
 int open(const char *path, int mode) {
+	char relpath[1024] = {0};
+	mountpoint_t *mp = NULL;
+	if (!find_relpath(path, relpath, &mp))
+		return -1;
+
 	assert(mode == O_RDONLY);
-	assert(path != NULL);
+	assert(mp->fops.open != NULL);
+	return mp->fops.open(mp->dev, relpath, mode);
+}
 
-	if (path[0] != '/')
-		return -1; // only absolute paths are supported
-
-	mountpoint_t *mp = find_mountpoint_for_path(path);
-	assert(mp != NULL);
+int stat(const char *path, struct stat *buf) {
+	assert(buf != NULL);
 
 	char relpath[1024] = {0};
+	mountpoint_t *mp = NULL;
+	if (!find_relpath(path, relpath, &mp))
+		return -1;
 
-	assert(strnicmp(path, mp->path, strlen(mp->path)) == 0); // First part of the part should be the mountpoint path
-
-	if (strcmp(mp->path, "/") == 0)
-		strlcpy(relpath, path, 1024);
-	else {
-		// Strip the mountpoint from the beginning
-		strlcpy(relpath, path + strlen(mp->path), 1024);
-		if (relpath[0] == 0)
-			strcpy(relpath, "/");
-	}
-
-	assert(mp->fops.open != NULL);
-
-	return mp->fops.open(mp->dev, relpath, mode);
+	assert(mp->fops.stat != NULL);
+	return mp->fops.stat(mp, relpath, buf);
 }
 
 int read(int fd, void *buf, int length) {
@@ -86,7 +81,6 @@ int read(int fd, void *buf, int length) {
 	mountpoint_t *mp = file->mp;
 	assert(mp != NULL);
 	assert(mp->fops.read != NULL);
-
 	return mp->fops.read(fd, buf, length);
 }
 
@@ -102,32 +96,16 @@ int close(int fd) {
 	return mp->fops.close(fd);
 }
 
-int stat(const char *path, struct stat *buf) {
-	assert(path != NULL);
-	assert(buf != NULL);
+int closedir(DIR *dir) {
+	assert(dir != NULL);
+	assert(dir->mp != NULL);
+	return dir->mp->fops.closedir(dir);
+}
 
-	if (path[0] != '/')
-		return -1; // only absolute paths are supported
-
-	mountpoint_t *mp = find_mountpoint_for_path(path);
-	assert(mp != NULL);
-
-	char relpath[1024] = {0};
-
-	assert(strnicmp(path, mp->path, strlen(mp->path)) == 0); // First part of the part should be the mountpoint path
-
-	if (strcmp(mp->path, "/") == 0)
-		strlcpy(relpath, path, 1024);
-	else {
-		// Strip the mountpoint from the beginning
-		strlcpy(relpath, path + strlen(mp->path), 1024);
-		if (relpath[0] == 0)
-			strcpy(relpath, "/");
-	}
-
-	assert(mp->fops.stat != NULL);
-
-	return mp->fops.stat(mp, relpath, buf);
+struct dirent *readdir(DIR *dir) {
+	assert(dir != NULL);
+	assert(dir->mp != NULL);
+	return dir->mp->fops.readdir(dir);
 }
 
 int chdir(const char *path) {
