ead30d98f2559a58cd0e6dd7b2d5d8d3987361d2
Thomas Backman <serenity@exscape.org>
2012-12-16 14:33:33 +0100

    Fix the guess-the-number game. (Very OS-devvy stuff!)

--- src/userspace/guess/guess.c
+++ src/userspace/guess/guess.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_GUESSES 10
+
 int main(int argc, char **argv) {
 	srand(848214); // TODO: time()
 	int num = 1 + (rand() % 100);
 	int guess = -1;
 	int num_guesses = 0;
 
+	char buf[16] = {0};
+
 	while (guess != num) {
-		num_guesses++;
+		printf("%d guesses remaining\n", MAX_GUESSES - num_guesses);
 		fputs("Guess the number, from user mode (1-100): ", stdout);
 		fflush(stdout);
 
-		scanf("%d", &guess);
+		char *end;
+		fgets(buf, 16, stdin);
+		guess = strtol(buf, &end, 10);
+		if (*end != 0 && *end != '\n') {
+			printf("That's not a number! Try again.\n");
+			continue;
+		}
+
+		num_guesses++;
 
 		if (guess == num) {
-			puts("You got it!");
+			printf("You got it on guess %d!\n", num_guesses);
 			break;
+		}
+		else if (num_guesses >= MAX_GUESSES) {
+			printf("Sorry, you lost! The number was %d.\n", num);
+			return 0;
 		}
 		else if (guess > num) {
 			puts("Nope. Try lower.");
