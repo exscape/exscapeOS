29c6986cdb02eff307b1ef8a6034523c623c839c
Thomas Backman <serenity@exscape.org>
2014-11-30 16:45:13 +0100

    Clean up/optimize copy_page_physical (rep movsd)

--- src/kernel/copy_page.s
+++ src/kernel/copy_page.s
@@ -3,34 +3,30 @@ align 4
 global copy_page_physical
 
 copy_page_physical:
-	push ebx    ; we can only trample eax, ecx and edx
 	pushf       ; push EFLAGS
 	cli
+	push esi    ; we can only trample eax, ecx and edx
+	push edi
 
-	mov ebx, [esp+12] ; source address
-	mov ecx, [esp+16] ; dest address
+	; NOTE: if the number of pushes above changes, these offsets change as well!
+	mov esi, [esp+16] ; source address
+	mov edi, [esp+20] ; dest address
 
 	; Disable paging
 	mov edx, cr0
 	and edx, 0x7fffffff
 	mov cr0, edx
 
-	mov edx, 1024 ; 1024*4 bytes to copy
-
-	.loop:
-	; TODO: rep movsd? performance should be important 'round these parts
-		mov eax, [ebx]
-		mov [ecx], eax
-		add ebx, 4
-		add ecx, 4
-		dec edx
-		jnz .loop
+	; Copy the data
+	mov ecx, 1024 ; 1024*4 bytes to copy
+	rep movsd
 
 	; Re-enable paging
 	mov edx, cr0
 	or edx, 0x80000000
 	mov cr0, edx
 
+	pop edi
+	pop esi
 	popf  ; return EFLAGS, which will also re-enable interrupts
-	pop ebx ; restore ebx
 	ret
