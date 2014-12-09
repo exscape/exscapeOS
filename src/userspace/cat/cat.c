db9e1549859d6aa5bae2da26b767fa275883dc12
Thomas Backman <serenity@exscape.org>
2014-12-09 09:38:01 +0100

    cat: support stdin (argc == 1 or filename "-")

--- src/userspace/cat/cat.c
+++ src/userspace/cat/cat.c
@@ -12,20 +12,27 @@
 #endif
 
 int main(int argc, char **argv) {
-	if (argc == 1)
-		return 0;
-
 #define BUFSIZE (16*1024)
 	char *buf = malloc(BUFSIZE);
 
 	bool error = false;
 
-	for (int i=1; i < argc; i++) {
-		int fd = open(argv[i], O_RDONLY);
-		if (fd < 0) {
-			fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
-			error = true;
-			continue;
+	for (int i=1; i < argc || i == 1; i++) {
+		const char *filename;
+		int fd;
+
+		if (argc == 1 || (argc > 1 && !strcmp(argv[i], "-"))) {
+			filename = "stdin";
+			fd = fileno(stdin);
+		}
+		else {
+			filename = argv[i];
+			fd = open(filename, O_RDONLY);
+			if (fd < 0) {
+				fprintf(stderr, "%s: %s: %s\n", argv[0], filename, strerror(errno));
+				error = true;
+				continue;
+			}
 		}
 
 		int r = 0;
@@ -34,7 +41,7 @@ int main(int argc, char **argv) {
 			memset(buf, 0, BUFSIZE);
 			r = read(fd, buf, BUFSIZE);
 			if (r == -1) {
-				fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
+				fprintf(stderr, "%s: %s: %s\n", argv[0], filename, strerror(errno));
 				break;
 			}
 			tot += r;
@@ -42,7 +49,8 @@ int main(int argc, char **argv) {
 				write(fileno(stdout), buf, r);
 		} while (r > 0);
 
-		close(fd);
+		if (!strcmp(filename, "stdin") != 0 && fd != fileno(stdin))
+			close(fd);
 	}
 
 	if (!error)
